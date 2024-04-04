a2c3f6d4f1dbd9431f90077917eb7da49ed55bea
Thoriqurrahman Akrami <thoriqurrahmna@gmail.com>
2024-04-04 11:52:19 +0700

    upload ulang ke main

--- album.php
+++ album.php
@@ -100,7 +100,7 @@
             <div class="card shadow-sm">
               <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
                 <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Muhidin Salimin</text>
+                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Muhidin Saimin</text>
               </svg>
 
               <div class="card-body">
